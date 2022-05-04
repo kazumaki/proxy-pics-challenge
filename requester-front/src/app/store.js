20cf1fca03b596761c9ce62a135573053a49b74b
Kazumaki <vcampos.pitangui@gmail.com>
2022-05-04 20:43:17 -0300

    add ordersSlice to store

--- requester-front/src/app/store.js
+++ requester-front/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authenticationSlice } from '../features/authentication/authenticationSlice';
 import { usersSlice } from '../features/users/usersSlice';
+import { ordersSlice } from '../features/orders/ordersSlice';
 
 export const store = configureStore({
   reducer: {
     users: usersSlice.reducer,
     authentication: authenticationSlice.reducer,
+    orders: ordersSlice.reducer,
   },
 });
