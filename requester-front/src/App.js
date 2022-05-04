8432030cb5a9a40de83d60e5b146d876001da046
Kazumaki <vcampos.pitangui@gmail.com>
2022-05-04 20:43:35 -0300

    refactor routes;

--- requester-front/src/App.js
+++ requester-front/src/App.js
@@ -13,15 +13,22 @@ import AuthenticateRoute from './features/authentication/AuthenticateRoute';
 import Login from './features/authentication/Login';
 
 function App() {
-  const { authentication } = useSelector(state => state);
+  const { authentication, users, orders } = useSelector(state => state);
 
   return (
     <div>
       <Header currentUserId={authentication.currentUserId}/>
       <Router>
         <Routes>
-          <Route path="/" element={<AuthenticateRoute component={Orders} />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={
+            <AuthenticateRoute component={
+              <Orders 
+                users={users.users}
+                orders={orders.requesterOrders}
+                currentUserId ={authentication.currentUserId} />
+            } />
+          } />
+          <Route path="/login" element={<Login users={users.users} />} />
         </Routes>
       </Router>
     </div>
