0eaca750414fa8b6b502879d8e9c178375826ecf
Kazumaki <vcampos.pitangui@gmail.com>
2022-05-05 00:20:21 -0300

    add basic app structure; add .env file reader

--- orders/App.js
+++ orders/App.js
@@ -6,107 +6,45 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import UsersList from './src/features/users/UsersList';
+import AuthenticateRoute from './src/features/authentication/AuthenticateRoute';
+import Login from './src/features/authentication/Login';
+import { View, Text } from 'react-native';
+import { NativeRouter, Route, Routes } from "react-router-native";
+import { fetchUsers } from './src/features/users/usersAPI';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const {users, authentication} = useSelector(state => state);
+  const dispatch = useDispatch();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  useEffect(() => {
+    if(authentication.currentUserId) {
+      // dispatch(fetchOrders());
+    }
+  }, [dispatch, authentication.currentUserId])
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NativeRouter>
+      <View>
+        <Routes>
+          <Route path="/" 
+          element={
+          <AuthenticateRoute currentUserId={authentication.currentUserId} component={<View><Text>TEst</Text></View>}
+          />
+          }
+        />
+        <Route path="/login" element={<Login users={users.users} currentUserId={authentication.currentUserId} />} />
+        </Routes>
+      </View>
+    </NativeRouter>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
