e8d8e111c932466bd7a518a810358737ebbef4e6
Kazumaki <vcampos.pitangui@gmail.com>
2022-05-08 17:59:43 -0300

    add some styling to ordersListItem

--- requester-front/src/features/orders/OrderItem.js
+++ requester-front/src/features/orders/OrderItem.js
@@ -3,24 +3,22 @@ import style from "./orderitem.module.css";
 
 const OrderItem = ({ order }) => {
   return (
-    <div className={style.orderContainer}>
-      <h5>Id: </h5>
-      <Link className={style.orderLink} to={`/orders/${order.id}`}>
-        {order.id}
-      </Link>
-      <h5>Date of creation: </h5>
-      <div>
-        {new Date(order.created_at).toLocaleDateString("en-US")}
-      </div>
-      <h5>Status: </h5>
-      <div>
-        {order.status}
-      </div>
-      <h5>Address: </h5>
-      <div>
-        {order.address}
-      </div>
-    </div>
+    <Link  to={`/orders/${order.id}`} className={style.orderContainer}>
+        <h5>Id: </h5>
+          {order.id}
+        <h5>Date of creation: </h5>
+        <div>
+          {new Date(order.created_at).toLocaleDateString("en-US")}
+        </div>
+        <h5>Status: </h5>
+        <div>
+          {order.status}
+        </div>
+        <h5>Address: </h5>
+        <div>
+          {order.address}
+        </div>
+    </Link>
   );
 }
 
